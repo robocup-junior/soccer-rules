195361e3e90291d2d4f7d2d2a23385adad985bb8
mr.Shu <mr@shu.io>
2017-09-26 21:32:56 +0200

    update: Redone the CI
    * Redone the CI in a hand-crafted bash script
    
    Signed-off-by: mr.Shu <mr@shu.io>

---
+++ .ci/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+# Taken from:
+# https://dfm.io/downloads/notebooks/travis-latex.ipynb
+
+TEX_DIRECTORY='tex/'
+
+if git diff --name-only $TRAVIS_COMMIT_RANGE | grep $TEX_DIRECTORY
+then
+  # Install tectonic using conda
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+  bash miniconda.sh -b -p $HOME/miniconda
+  export PATH="$HOME/miniconda/bin:$PATH"
+  hash -r
+  conda config --set always_yes yes --set changeps1 no
+  conda update -q conda
+  conda info -a
+  conda create --yes -n paper
+  source activate paper
+  conda install -c conda-forge -c pkgw-forge tectonic
+
+  # Build the paper using tectonic
+  cd $TEX_DIRECTORY
+  tectonic *.tex --print
+
+  # Force push the paper to GitHub
+  cd $TRAVIS_BUILD_DIR
+  git checkout $TRAVIS_BRANCH
+  git -c user.name='travis' -c user.email='travis' commit -m "building the tex" -a
+  git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
+fi
