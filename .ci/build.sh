a2fb7b93559ff69d36490fc6408a8e3528960792
mr.Shu <mr@shu.io>
2017-09-27 12:20:47 +0200

    update: Add building bot messages to PRs
    * Add a bot that will add a comment to PRs stating what has been built
      and where can people check it out.
    
    * Add a link to README
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- .ci/build.sh
+++ .ci/build.sh
@@ -6,13 +6,16 @@
 set -e
 
 TEX_DIRECTORY='tex/'
+TEX_FILENAME='sample'
 COMMIT_USERNAME='Travis the TeX builer'
 COMMIT_EMAIL='travis@travis.ai'
 
 # If the commit range does not contain two commits (with '..' in between them),
 # do the diff against the previous commit in the history line.
 if ! [[ $TRAVIS_COMMIT_RANGE == *..* ]]; then
-  TRAVIS_COMMIT_RANGE="HEAD~1"
+  TRAVIS_START_COMMIT=$(git rev-parse HEAD~1)
+  TRAVIS_FINISH_COMMIT=$(git rev-parse HEAD)
+  TRAVIS_COMMIT_RANGE="$TRAVIS_START_COMMIT..$TRAVIS_FINISH_COMMIT"
 fi
 
 if git diff --name-only $TRAVIS_COMMIT_RANGE | grep $TEX_DIRECTORY | grep '.tex$'
@@ -31,14 +34,19 @@ then
 
   # Build the paper using tectonic
   cd $TEX_DIRECTORY
-  tectonic *.tex --print
+  tectonic $TEX_FILENAME.tex --print
 
   # Force push the paper to GitHub
   cd $TRAVIS_BUILD_DIR
-  git checkout $TRAVIS_BRANCH
-  git add $TEX_DIRECTORY/*.tex
+  git checkout --orphan $TRAVIS_BRANCH-pdf
+  git rm -rf .
+  git add -f $TEX_DIRECTORY/$TEX_FILENAME.pdf
   git -c user.name="$COMMIT_USERNAME"\
       -c user.email="$COMMIT_EMAIL"\
       commit -am "Built the tex from $TRAVIS_COMMIT_RANGE"
-  git push -q -f https://$GITHUB_USERNAME:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH
+  git push -q -f https://$GITHUB_USERNAME:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
+
+  pip install travis_bot
+
+  echo "Check the latest version of the built PDF at https://github.com/$TRAVIS_REPO_SLUG/blob/$TRAVIS_BRANCH-pdf/$TEX_FILENAME.pdf" | travis_bot
 fi
