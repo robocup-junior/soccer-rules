9df737e5c5815c73f716f63b1e4836fd242a0a44
Marek Šuppa <mrshu@users.noreply.github.com>
2017-09-28 22:37:31 +0200

    update: A small test change (#10)
    * update: A small test change
    
    * A small test change.
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: Fix CI script
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: Install PIP using conda
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: Some even more updates
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: Small test update
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: Another update
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: CI Update
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: And one last change.
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: Just one small thing
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: another test.
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: Another test
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: Add travis bot
    
    * Add travis bot
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: Fix buid.sh
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: Fix travis bot
    
    Signed-off-by: mr.Shu <mr@shu.io>
    
    * update: Message update
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- .ci/build.sh
+++ .ci/build.sh
@@ -10,12 +10,22 @@ TEX_FILENAME='sample'
 COMMIT_USERNAME='Travis the TeX builer'
 COMMIT_EMAIL='travis@travis.ai'
 
-# If the commit range does not contain two commits (with '..' in between them),
-# do the diff against the previous commit in the history line.
-if ! [[ $TRAVIS_COMMIT_RANGE == *..* ]]; then
-  TRAVIS_START_COMMIT=$(git rev-parse HEAD~1)
-  TRAVIS_FINISH_COMMIT=$(git rev-parse HEAD)
-  TRAVIS_COMMIT_RANGE="$TRAVIS_START_COMMIT..$TRAVIS_FINISH_COMMIT"
+# # If the commit range does not contain two commits (with '..' in between them),
+# # do the diff against the previous commit in the history line.
+# if ! [[ $TRAVIS_COMMIT_RANGE == *..* ]]; then
+#   TRAVIS_START_COMMIT=$(git rev-parse HEAD~1)
+#   TRAVIS_FINISH_COMMIT=$(git rev-parse HEAD)
+#   TRAVIS_COMMIT_RANGE="$TRAVIS_START_COMMIT..$TRAVIS_FINISH_COMMIT"
+# fi
+
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+  if ! [[ $TRAVIS_BRANCH == "master" ]]; then
+     echo "We do not build on pushes for any other branch than 'master'."
+     exit 0;
+  fi
+else
+  # override the TRAIVS_BRANCH to the one from which the PR originates
+  export TRAVIS_BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
 fi
 
 if git diff --name-only $TRAVIS_COMMIT_RANGE | grep $TEX_DIRECTORY | grep '.tex$'
@@ -30,7 +40,7 @@ then
   conda info -a
   conda create --yes -n paper
   source activate paper
-  conda install -c conda-forge -c pkgw-forge tectonic
+  conda install -c conda-forge -c pkgw-forge tectonic pip
 
   # Build the paper using tectonic
   cd $TEX_DIRECTORY
@@ -38,15 +48,22 @@ then
 
   # Force push the paper to GitHub
   cd $TRAVIS_BUILD_DIR
+
   git checkout --orphan $TRAVIS_BRANCH-pdf
-  git rm -rf .
   git add -f $TEX_DIRECTORY/$TEX_FILENAME.pdf
   git -c user.name="$COMMIT_USERNAME"\
       -c user.email="$COMMIT_EMAIL"\
       commit -am "Built the tex from $TRAVIS_COMMIT_RANGE"
   git push -q -f https://$GITHUB_USERNAME:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 
-  pip install travis_bot
+  pip install requests
 
-  echo "Check the latest version of the built PDF at https://github.com/$TRAVIS_REPO_SLUG/blob/$TRAVIS_BRANCH-pdf/$TEX_FILENAME.pdf" | travis_bot
+  export TRAVIS_BOT_NO_RESULTS_MSG="Check the latest version of the [built PDF](https://github.com/$TRAVIS_REPO_SLUG/blob/$TRAVIS_BRANCH-pdf/tex/$TEX_FILENAME.pdf)!"
+
+  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+  echo $TRAVIS_BOT_NO_RESULTS_MSG;
+
+  echo $TRAVIS_BOT_NO_RESULTS_MSG | python .ci/travis_bot.py
+
 fi
