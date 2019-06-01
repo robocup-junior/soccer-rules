f9a12e8795ad661120bf4305674b48a7c9c09fca
mr.Shu <mr@shu.io>
2019-06-01 22:32:29 +0200

    .travis.yml: Add Criticmarkup support
    * Add Criticmarkup support to the Travis pipeline
    
    Signed-off-by: mr.Shu <mr@shu.io>

---
+++ .ci/criticmarkup_to_adoc.py
@@ -0,0 +1,134 @@
+import re
+import sys
+
+DELETION_REGEXP = re.compile(r'\{--(.*?)--\}')
+ADDITION_REGEXP = re.compile(r'\{\+\+(.*?)\+\+\}')
+SUBSTITUTION_REGEXP = re.compile(r'\{~~(.*?)~>(.*?)~~\}')
+
+
+class Deletor:
+
+    def __init__(self, changes, note_fmt, replacement_fmt):
+        self.n_deletions = 0
+        self.changes = changes
+        self.note_fmt = note_fmt
+        self.replacement_fmt = replacement_fmt
+
+    def __call__(self, match):
+        self.n_deletions += 1
+
+        def callback(n_deletions, match):
+            m = match.group(1)
+            txt = self.replacement_fmt.replace('{PREVIOUS}', m)
+            note = self.note_fmt.replace('{PREVIOUS}', m)
+            change_id = f'deletion-{n_deletions}'
+            self.changes.append(change_id)
+            return f'[[{change_id}, {note}]] {txt}'
+
+        return callback(self.n_deletions, match)
+
+
+class Additor:
+
+    def __init__(self, changes, note_fmt, replacement_fmt):
+        self.n_additions = 0
+        self.changes = changes
+        self.note_fmt = note_fmt
+        self.replacement_fmt = replacement_fmt
+
+    def __call__(self, match):
+        self.n_additions += 1
+
+        def callback(n_additions, match):
+            m = match.group(1)
+            txt = self.replacement_fmt.replace('{CURRENT}', m)
+            note = self.note_fmt.replace('{CURRENT}', m)
+            change_id = f'addition-{n_additions}'
+            self.changes.append(change_id)
+            return f'[[{change_id}, {note}]] {txt}'
+
+        return callback(self.n_additions, match)
+
+
+class Substituter:
+
+    def __init__(self, changes, note_fmt, replacement_fmt):
+        self.n_substitutions = 0
+        self.changes = changes
+        self.note_fmt = note_fmt
+        self.replacement_fmt = replacement_fmt
+
+    def __call__(self, match):
+        self.n_substitutions += 1
+
+        def callback(n_substitutions, match):
+            previous = match.group(1)
+            current = match.group(2)
+            txt = self.replacement_fmt.replace('{CURRENT}', current) \
+                                      .replace('{PREVIOUS}', previous)
+            note = self.note_fmt.replace('{CURRENT}', current) \
+                                .replace('{PREVIOUS}', previous)
+
+            change_id = f'substitution-{n_substitutions}'
+            self.changes.append(change_id)
+            return f'[[{change_id}, {note}]] {txt}'
+
+        return callback(self.n_substitutions, match)
+
+
+class CriticMarkupPreprocessor:
+
+    def __init__(self,
+                 change_listing_fmt='- {CHANGE}',
+                 addition_note_fmt='Added "{CURRENT}"',
+                 addition_replacement_fmt='{CURRENT}',
+                 deletion_note_fmt='Deleted "{PREVIOUS}"',
+                 deletion_replacement_fmt='(used to be "{PREVIOUS}")',
+                 substitution_note_fmt='Changed "{PREVIOUS}" to "{CURRENT}"',
+                 substitution_replacement_fmt='{CURRENT} (used to be "{PREVIOUS}")'): # noqa
+        self.changes = []
+        self.change_listing_fmt = change_listing_fmt
+        self.addition_note_fmt = addition_note_fmt
+        self.addition_replacement_fmt = addition_replacement_fmt
+        self.deletion_note_fmt = deletion_note_fmt
+        self.deletion_replacement_fmt = deletion_replacement_fmt
+        self.substitution_note_fmt = substitution_note_fmt
+        self.substitution_replacement_fmt = substitution_replacement_fmt
+
+    def convert(self, infile):
+        d = Deletor(self.changes,
+                    self.deletion_note_fmt,
+                    self.deletion_replacement_fmt)
+        a = Additor(self.changes,
+                    self.addition_note_fmt,
+                    self.addition_replacement_fmt)
+        s = Substituter(self.changes,
+                        self.substitution_note_fmt,
+                        self.substitution_replacement_fmt)
+
+        with open(infile) as f:
+            instr = f.read()
+            instr = DELETION_REGEXP.sub(d, instr)
+            instr = ADDITION_REGEXP.sub(a, instr)
+            instr = SUBSTITUTION_REGEXP.sub(s, instr)
+
+        list_of_changes = [self.change_listing_fmt.replace('{CHANGE}',
+                                                           change)
+                           for change in self.changes]
+        changes = '\n'.join(list_of_changes)
+
+        instr = instr.replace('{+-~TOC-CHANGES~-+}', changes)
+        return instr
+
+
+if __name__ == '__main__':
+    cmp = CriticMarkupPreprocessor(
+        change_listing_fmt='- <<{CHANGE}>>',
+        addition_note_fmt='Added "{CURRENT}"',
+        addition_replacement_fmt='`{CURRENT}`',
+        deletion_note_fmt='Deleted "{PREVIOUS}"',
+        deletion_replacement_fmt='footnote:[In previous version this said "{PREVIOUS}"]', # noqa
+        substitution_note_fmt='Changed "{PREVIOUS}" to "{CURRENT}"',
+        substitution_replacement_fmt='`{CURRENT}` footnote:[In previous version this said "{PREVIOUS}"]' # noqa
+    )
+    print(cmp.convert(sys.argv[1]))
