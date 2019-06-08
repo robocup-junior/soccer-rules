db1bb781d8621307d5679bf1a11accb86fca301a
mr.Shu <mr@shu.io>
2019-06-08 21:23:01 +0200

    travis: Move steps to after_script.sh
    * Move steps from the YAML file to a separate shell script, one that was
      previously used for the whole pipeline.
    
    Signed-off-by: mr.Shu <mr@shu.io>

---
+++ .ci/after_script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -x
+# Taken from:
+# https://dfm.io/downloads/notebooks/travis-latex.ipynb
+
+# Exit on non-zero return codes
+set -e
+
+TEX_DIRECTORY='tex/'
+TEX_FILENAME='rules'
+COMMIT_USERNAME='Travis the AsciiDoc builder'
+COMMIT_EMAIL='travis@travis.ai'
+
+# # If the commit range does not contain two commits (with '..' in between them),
+# # do the diff against the previous commit in the history line.
+# if ! [[ $TRAVIS_COMMIT_RANGE == *..* ]]; then
+#   TRAVIS_START_COMMIT=$(git rev-parse HEAD~1)
+#   TRAVIS_FINISH_COMMIT=$(git rev-parse HEAD)
+#   TRAVIS_COMMIT_RANGE="$TRAVIS_START_COMMIT..$TRAVIS_FINISH_COMMIT"
+# fi
+
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+  if ! [[ $TRAVIS_BRANCH == "master" ]]; then
+     echo "We do not build on pushes for any other branch than 'master'."
+     exit 0;
+  fi
+else
+  if [[ $GITHUB_USERNAME == "" ]]; then
+     echo "Seems like this PR came from a fork. For security reasons no code will be executed."
+     exit 0;
+  fi
+  # override the TRAIVS_BRANCH to the one from which the PR originates
+  export TRAVIS_BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
+fi
+
+if git diff --name-only $TRAVIS_COMMIT_RANGE | grep '.adoc$'
+then
+
+  git clone https://github.com/$TRAVIS_REPO_SLUG.git --single-branch --branch gh-pages $TRAVIS_BUILD_DIR/gh-pages
+
+  cd $TRAVIS_BUILD_DIR/gh-pages
+  mkdir $TRAVIS_BRANCH/
+  cp -R ../media ../*.html ../*.pdf $TRAVIS_BRANCH/
+
+  git config user.name "$COMMIT_USERNAME"
+  git config user.email "$COMMIT_EMAIL"
+
+  git add -f $TRAVIS_BRANCH
+  git commit -m "Update GitHub Pages $TRAVIS_COMMIT_RANGE"
+  git push -q -f "https://$GITHUB_USERNAME:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG" gh-pages
+
+  pip install requests
+  echo "See what the rules would look like in [the last draft](https://robocupjuniortc.github.io/soccer-rules/$TRAVIS_BRANCH/rules.html)!" | python ../.ci/travis_bot.py
+
+fi
