96f3d9677dbd1a31488e30b0252f247c4182f32a
mr.Shu <mr@shu.io>
2019-12-15 02:09:59 +0100

    update: Add SuperTeam rules
    * Add a SuperTeam rules for 2019
    
    * Ensure they get converted to PDF via the Travis CI
    
    Signed-off-by: mr.Shu <mr@shu.io>

---
+++ .ci/make_rules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+cp $1.adoc _$1.adoc
+python .ci/criticmarkup_to_adoc.py _$1.adoc > $1.adoc
+asciidoctor $1.adoc
+asciidoctor -b docbook $1.adoc
+dblatex -T db2latex $1.xml -t tex --texstyle=./manual.sty -p custom.xsl
+cp $1.tex /tmp/
+cat $1.tex | awk 'f;/\\mainmatter/{f=1}'  > $1_without_preamble.tex
+cat preamble.tex $1_without_preamble.tex > $1.tex
+texliveonfly $1.tex
+pdflatex $1.tex
+pdflatex $1.tex
