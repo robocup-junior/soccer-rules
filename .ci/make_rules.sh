925e9d8116e767aa83cad5ebc4a38cc64b621246
mr.Shu <mr@shu.io>
2019-12-15 02:16:28 +0100

    make_rules: Ensure asciidoc is docker-run
    * Make sure asciidoc is run from Docker
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- .ci/make_rules.sh
+++ .ci/make_rules.sh
@@ -2,8 +2,8 @@
 
 cp $1.adoc _$1.adoc
 python .ci/criticmarkup_to_adoc.py _$1.adoc > $1.adoc
-asciidoctor $1.adoc
-asciidoctor -b docbook $1.adoc
+docker run -v $TRAVIS_BUILD_DIR:/documents/ asciidoctor/docker-asciidoctor asciidoctor $1.adoc
+docker run -v $TRAVIS_BUILD_DIR:/documents/ asciidoctor/docker-asciidoctor asciidoctor -b docbook $1.adoc
 dblatex -T db2latex $1.xml -t tex --texstyle=./manual.sty -p custom.xsl
 cp $1.tex /tmp/
 cat $1.tex | awk 'f;/\\mainmatter/{f=1}'  > $1_without_preamble.tex
