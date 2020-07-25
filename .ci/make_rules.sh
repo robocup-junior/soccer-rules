bb617ec71966ffa63f8fd325130e27e7f2a9f38e
mr.Shu <mr@shu.io>
2020-07-25 19:47:28 +0200

    ci: Improve make_rules script
    * Allow the `make_rules` script to make use of `podman` as well as
      Docker, and make it so that Podman is by default attaching :Z to
      volume names so that they work with SELinux
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- .ci/make_rules.sh
+++ .ci/make_rules.sh
@@ -1,13 +1,30 @@
-#!/bin/bash -x
+#!/bin/bash
+set -euo pipefail
 
-cp $1.adoc _$1.adoc
-python .ci/criticmarkup_to_adoc.py _$1.adoc > $1.adoc
-docker run -v $TRAVIS_BUILD_DIR:/documents/ asciidoctor/docker-asciidoctor asciidoctor $1.adoc
-docker run -v $TRAVIS_BUILD_DIR:/documents/ asciidoctor/docker-asciidoctor asciidoctor -b docbook $1.adoc
-dblatex -T db2latex $1.xml -t tex --texstyle=./manual.sty -p custom.xsl
-cp $1.tex /tmp/
-cat $1.tex | awk 'f;/\\mainmatter/{f=1}'  > $1_without_preamble.tex
-cat preamble.tex $1_without_preamble.tex > $1.tex
-texliveonfly $1.tex
-pdflatex $1.tex
-pdflatex $1.tex
+DOCKER_COMMAND=docker
+VOLUME_SUFFIX=
+PODMAN_BUILD=${PODMAN_BUILD:-no}
+
+if [[ $PODMAN_BUILD == "yes" ]]; then
+        DOCKER_COMMAND="podman"
+        VOLUME_SUFFIX=":Z"
+fi
+
+OUTPUT_FILE=$1
+cp $1.adoc tmp_$1.adoc
+OUTPUT_PREFIX=tmp_$1
+
+cp $OUTPUT_PREFIX.adoc _$OUTPUT_PREFIX.adoc
+python .ci/criticmarkup_to_adoc.py _$OUTPUT_PREFIX.adoc > $OUTPUT_PREFIX.adoc
+$DOCKER_COMMAND run -v $TRAVIS_BUILD_DIR:/documents/$VOLUME_SUFFIX asciidoctor/docker-asciidoctor asciidoctor $OUTPUT_PREFIX.adoc
+$DOCKER_COMMAND run -v $TRAVIS_BUILD_DIR:/documents/$VOLUME_SUFFIX asciidoctor/docker-asciidoctor asciidoctor -b docbook $OUTPUT_PREFIX.adoc
+dblatex -T db2latex $OUTPUT_PREFIX.xml -t tex --texstyle=./manual.sty -p custom.xsl
+
+# Go through the generated .tex output, find the place where the preamble ends
+# (marked by the \mainmatter command) and create a file without it.
+cat $OUTPUT_PREFIX.tex | awk 'f;/\\mainmatter/{f=1}'  > $OUTPUT_PREFIX"_without_preamble.tex"
+# Concat the standardized preamble with the "without_preamble" version of the file
+cat preamble.tex $OUTPUT_PREFIX"_without_preamble.tex" > $OUTPUT_PREFIX.tex
+texliveonfly $OUTPUT_PREFIX.tex
+pdflatex $OUTPUT_PREFIX.tex -jobname=$OUTPUT_FILE
+pdflatex $OUTPUT_PREFIX.tex -jobname=$OUTPUT_FILE
